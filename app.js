6a79b17bd0cd65a62b08438c211952ce1d0acd47
AsadMarfani <asad_marfani@outlook.com>
2017-12-11 13:44:17 +0500

    Added Passportjs module in to rest-server

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var config =require('./config');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
 
 var mongoose = require('mongoose');
-var URL = "mongodb://localhost:27017/conFusion";
-mongoose.connect(URL);
+// var URL = "mongodb://localhost:27017/conFusion";
+mongoose.connect(config.mongoURL);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
@@ -30,6 +33,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
+
+//passport config
+var User = require('./models/user');
+app.use(passport.initialize());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
@@ -48,14 +59,11 @@ app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req
-    .app
-    .get('env') === 'development'
-    ? err
-    : {};
+    .app.get('env') === 'development'? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json('error');
 });
 
 module.exports = app;
